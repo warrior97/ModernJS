f18c218598d344949ba2e6bc9452f61768f14916
warrior97 <bobicicd@hotmail.com>
2019-10-16 12:43:13 +0200

    added bare project

---
+++ bare-project/src/index.js
@@ -0,0 +1,9 @@
+//import '../assets/css/style.css';
+
+
+
+//dan 1
+console.log('Hello by js');
+
+const app = document.getElementById('app');
+app.innerHTML = '<p>Hello to BootcampPizza by JavaScript Basics</p>';
